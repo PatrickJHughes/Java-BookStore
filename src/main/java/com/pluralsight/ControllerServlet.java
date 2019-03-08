2b4219502d3421e497a90ec9dbc37445767ce117
Patrick Hughes <p.hughes@edaptive.com>
2019-03-07 20:58:49 -0500

    Completed the module

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -65,6 +65,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/edit":
 					showEditForm(request, response);
 		  break;
+				case "/update":
+					updateBook(request, response);
+		  break;
         default:
 				   listBooks(request, response);
            break;
@@ -123,6 +126,16 @@ public class ControllerServlet extends HttpServlet {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
 		request.setAttribute("book", existingBook);
 		dispatcher.forward(request, response);
+	}
+	
+	private void updateBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		String title = request.getParameter("bookatitle");
+		String author = request.getParameter("bookauthor");
+		String price = request.getParameter("bookprice");
+		Book newBook = new Book(id, title, author, Float.parseFloat(price));
+		bookDAO.updateBook(newBook);
+		response.sendRedirect("list");
 	}
 
 	/**
